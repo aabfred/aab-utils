06af22547a4a1e3d5a93f668ec1c0c8336669c9f
Frédéric Declercq <frederic.declercq@aab.life>
2018-05-14 13:24:38 +0200

    [++] HeadersParser

--- README.md
+++ README.md
@@ -1,2 +1,144 @@
 # aab-utils
 a@b framework - Utils
+
+Aim:
+* implement base features for both browser & nodeJS
+* port browser features to nodeJS to enhance hybrid development
+
+## Browser use
+As this module implements features directly to **global**, you should declare it in browser before loading libraries.
+
+On window context:
+
+    window.global = window;
+
+On worker context:
+
+    self.global = self;
+
+## aab-utils/headersparser
+This script will implement **HeadersParser** class in **global**.
+
+There's no use to implement any message headers declared in IANA, because no script requires all of them, and syntax may differ with protocol. HeadersParser simply deals with syntax ( URL encoded tokens, quoted strings ... ) and offers an easy way to declare parsing rules.
+
+    const parser = new HeadersParser({
+        "content-type": { parse: [ "keyattrs", ";", "mime" ], cas: "lower" },
+        "content-length": { key: "length", parse: "number" },
+        "content-encoding": { key: "encodings", parse: [ "list", "," ] },
+        "content-language": { key: "languages", parse: [ "list", "," ] },
+    });
+    ...
+    const content = parser.parse( headers );
+
+In example above, we've declared a parser that can be used later to compute some headers into content variable. **key** is not defined on **content-type** because we want resulting object to be assigned to result.
+
+    parser.parse({
+        "content-type": 'multipart/form-data; charset="UTF-8"; boundary=---------------------------974767299852498929531610575',
+        "content-length": "68137",
+        "content-encoding": "deflate, gzip",
+        "content-language": "de-DE, en-CA"
+    })
+
+... will produce:
+
+    {
+        charset: 'utf-8',
+        boundary: '---------------------------974767299852498929531610575',
+        mime: 'multipart/form-data',
+        length: 68137,
+        encodings: [ 'deflate', 'gzip' ],
+        languages: [ 'de-DE', 'en-CA' ]
+    }
+
+### Common use
+#### new HeadersParser( conf )
+Entries in conf parameter are treated with **HeadersParser.prototype.set( name, options )**.
+
+#### HeadersParser.prototype.set( name, options={} )
+Options may contain:
+* cas = [ "upper" | "lower" ] case
+* key = object key for resulting value
+* multi = may have multiple declarations ( as Set-Cookie header )
+* parse = method to parse value
+
+**parse** parameter may be:
+* a string or symbol pointing to any parser method ( "decode" by default if not parse )
+* a function
+* true = don't parse anything
+* an array as [ parser method, ...arguments ]
+* an object to map resulting values
+
+Anyway, **parse** parameter will be stored as a function called like this:
+
+    item.parse.call( parser, text optionnaly altered with cas param )
+
+### Methods for parse parameter
+#### HeadersParser.prototype.decode( text )
+Default method when Boolean( parse parameter ) returns false.
+
+Returns a string using **split( keep=true )**, so spaces will be converted to single space and tokens will be converted by decodeURIComponent and optionnaly quoted.
+
+    { "user-agent": {} }
+
+#### HeadersParser.prototype.number( text )
+Returns a number.
+
+    { "content-length": { parse: "number" } }
+
+#### HeadersParser.prototype.date( text )
+Returns a date.
+
+    { "date": { parse: "date" } }
+
+#### HeadersParser.prototype.list( sep, text )
+Groups elements in arrays
+
+    { "content-encoding": { parse: [ "list", "," ] } }
+
+**sep** may be an array
+
+    { "accept": { parse: [ "list", [ ",", ";" ] ] } }
+
+#### HeadersParser.prototype.attrs( sep, text )
+Returns an object.
+
+    { "cookie": { parse: [ "attrs", "," ] } }
+
+#### HeadersParser.prototype.keyattrs( sep, name, text )
+Returns an object providing a name for the first value.
+
+    { "content-type": { parse: [ "keyattrs", ";", "mime" ] } }
+
+#### HeadersParser.prototype.map( map, text )
+Returns a mapped value ( used when parse is an object ).
+
+    { "x-dns-prefectch-control": { cas: "lower", parse: { "on": true, "off": false } } }
+
+### Further methods & properties
+#### HeadersParser.prototype.split( keep, text )
+If keep is true, elements will keep spaces converted to single space, quoted for quoted strings, and will be quoted on tokens converted by decodeURIComponent when value contains spaces.
+
+
+This method splits text into and array of elements using **HeadersParser.SPLITS** predefined methods.
+
+#### HeadersParser.prototype.nextString( index, text )
+Returns next quoted string or token ( with keep = false )
+
+#### HeadersParser.SPLITS
+Array of methods used by split method:
+* delimiters ( one character per element in **,/:;=?@\\** )
+* tokens ( with decodeURIComponent )
+* quoted strings
+* comments ( parenthesis )
+* brackets
+* curly brackets
+* tags ( less & more brackets )
+
+#### HeadersParser.quote( text )
+Adds quotes escaping inner quotes
+
+#### HeadersParser.unquote( text )
+Removes quotes unescaping inner quotes
+
+#### HeadersParser.unchanged( text )
+Returns text
---
+++ headersparser.js
@@ -0,0 +1,209 @@
+const
+    TOKEN = "!#$%&'*+-.^_`|~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    SPACE = " \t\r\n",
+    DELIMITERS = ",/:;=?@\\",
+    LAST = "\x7f",
+
+    headers2pairs = {
+        object: x => Object.entries(x),
+        pairs: x => x,
+        raw: x => {
+            const pairs = [];
+            for( let i=0; i<x.length; i+=2 )
+                pairs.push( x.slice( i, i+2 ) );
+            return pairs;
+        }
+    },
+    unchanged = x => x,
+    quote = x => `"${ x.replace(/"/g, '\\"') }"`,
+    unquote = x => x.slice( 1, -1 ).replace(/\\"/g, '"'),
+    
+    brackets = ( start, end, idx, txt, parts ) => {
+        let i = idx;
+        if( txt[i++] !== start ) return;
+        let n=1;
+        while( n && ( i < txt.length ) ){
+            switch( txt[i] ){
+                case end: n--; break;
+                case start: n++; break;
+                case "\\": i++; break;
+            }
+            i++;
+        }
+        parts.push( txt.slice( idx, i ) );
+        return i;
+    },
+    space = ( idx, txt, parts ) => {
+        let i = idx;
+        while( ( i < txt.length ) && SPACE.includes( txt[i] ) ) ++i;
+        if( parts && ( i > idx ) ) parts.push(" ");
+        return i;
+    },
+    token = ( idx, txt, parts, keep ) => {
+        let v, i = idx;
+        while( ( i < txt.length ) && (
+            TOKEN.includes( v = txt[i] ) ||
+            ( v > LAST )
+        )) ++i;
+        if( i == idx ) return;
+        v = decodeURIComponent( txt.slice( idx, i ) );
+        parts.push( keep && v.includes(" ")? quote(v) : v );
+        return i;
+    },
+    delimiter = ( idx, txt, parts ) => {
+        const c = txt[idx];
+        if( !DELIMITERS.includes( c ) ) return;
+        parts.push( c );
+        return idx + 1;
+    },
+    quotes = ( idx, txt, parts, keep ) => {
+        const
+            a = [],
+            i = brackets( '"', '"', idx, txt, a );
+        if( !i ) return;
+        parts.push( keep? a[0] : unquote(a[0]) );
+        return i;
+    },
+
+    list = ( seps, parts ) => {
+        if( !Array.isArray(seps) ) seps = [ seps ];
+        let sep, s=0;
+        [ sep, ...seps ] = seps;
+        const result = [];
+        for( let e = 0; e < parts.length; e++ ){
+            if( parts[e] !== sep ) continue;
+            result.push( parts.slice( s, e ) );
+            s = e + 1;
+        }
+        if( s < parts.length ) result.push( parts.slice( s ) );
+        return seps.length? result.map( list.bind( null, seps ) ) : result;
+    },
+    attrs = (o, [k, op, v]) => {
+        if( op == "=" ) o[k] = v;
+        else throw new SyntaxError(`attrs: op should be "=", found "${ op }"`);
+        return o;
+    };
+
+
+class HeadersParser {
+
+    constructor( conf ){
+        this.HEADERS = [];
+        if( conf )
+            for( const [ name, options ] of Object.entries( conf ) )
+                this.set( name, options );
+    }
+    set( header, options={} ){
+        let { key, multi, parse, cas } = options;
+        const conf = { header };
+        if( key ) conf.key = key;
+        if( multi ) conf.multi = true;
+        if( cas == "upper" ) conf.cas = "toUpperCase";
+        if( cas == "lower" ) conf.cas = "toLowerCase";
+        if( !parse ) parse = "decode";
+        switch( typeof parse ){
+            case "string":
+            case "symbol":
+                if( parse in this )
+                    conf.parse = this[ parse ].bind( this );
+                else
+                    throw new SyntaxError(`parser[${ parse }] isn't defined`);
+                break;
+            case "function":
+                conf.parse = parse;
+                break;
+            case "boolean":
+                conf.parse = unchanged;
+                break;
+            case "object":
+                if( Array.isArray( parse ) ) conf.parse = this[ parse[0] ].bind( this, ...parse.slice(1) );
+                else conf.parse = this.map.bind( this, parse );
+                break;
+            default:
+                throw new SyntaxError(`parse with type ${ typeof parse } isn't supported`);
+        }
+        this.HEADERS.push( conf );
+        return this;
+    }
+
+    parse( headers, type ){
+        if( !type ) type = Array.isArray( headers )? "pairs" : "object";
+        const
+            result = {},
+            pairs = headers2pairs[ type ]( headers );
+        for( const { header, key, multi, parse, cas } of this.HEADERS ){
+            let values = pairs.reduce( (a,[k,v]) => k == header? a.concat(v) : a, [] );
+            if( values.length ){
+                if( cas ) values = values.map( x => x[cas]() );
+                if( multi ) values = values.map( parse.bind( this ) );
+                else values = parse.call( this, values[0] );
+                if( key ) result[ key ] = values;
+                else Object.assign( result, values );
+            }
+        }
+        return result;
+    }
+
+    split( keep, txt ){
+        const parts = [];
+        let i=0;
+        while( i < txt.length ){
+            i = space( i, txt, keep && parts );
+            if( i == txt.length ) break;
+            let n;
+            for( const b of this.constructor.SPLITS )
+                if( ( n = b.call( this, i, txt, parts, keep ) ) ) break;
+            if( !n ) throw Object.assign( new SyntaxError(`No a regular header value: "${ txt }" ${ parts }`), { code: "EPARSE" });
+            i = n;
+        }
+        return parts;
+    }
+    nextString( idx, txt ){
+        const parts = [];
+        let i = token( idx, txt, parts );
+        if( !i ) i = quotes( idx, txt, parts );
+        if( i ) return { index: i, text: parts[0] };
+    }
+    decode( txt ){
+        const parts = this.split( true, txt );
+        return (parts.length == 1) && (parts[0][0] == '"')? unquote(parts[0]) : parts.join("");
+    }
+    number( txt ){
+        return Number( txt.replace(/[\s"]+/, "") );
+    }
+    date( txt ){
+        return new Date( txt.trim().replace(/"+/, "") );
+    }
+    map( map, txt ){
+        return map[ this.decode( txt ) ];
+    }
+    list( sep, txt ){
+        return list( sep, this.split( false, txt ) ).map( x => x.join("") );
+    }
+    attrs( sep, txt ){
+        return list( sep, txt ).reduce( attrs, Object.create( null ) );
+    }
+    keyattrs( sep, name, txt ){
+        const
+            l = list( sep, this.split( false, txt ) ),
+            value = l.shift().join(""),
+            atrs = l.reduce( attrs, Object.create( null ) );
+        atrs[ name ] = value;
+        return atrs;
+    }
+    
+}
+HeadersParser.quote = quote;
+HeadersParser.unquote = unquote;
+HeadersParser.unchanged = unchanged;
+HeadersParser.SPLITS = [
+    delimiter,
+    token,
+    quotes,
+    brackets.bind( null, "(", ")" ),
+    brackets.bind( null, "[", "]" ),
+    brackets.bind( null, "{", "}" ),
+    brackets.bind( null, "<", ">" )
+];
+
+global.HeadersParser = HeadersParser;
